7fe73025442e1cd7fdba6bee21653210c213047d
Daniel Kondratiuk <venga1.ua@gmail.com>
2021-01-27 04:19:08 +0100

    Lesson 13 Slider added Dots

--- src/js/slider.v2.js
+++ src/js/slider.v2.js
@@ -9,43 +9,55 @@ const slider = document.querySelector('.offer__slider'),
 		currentSlideImg = slider.querySelector('#current'),
 		width = window.getComputedStyle(sliderWrapper).width
 
+//Css style
+slider.style.position = 'relative'
 sliderField.style.width = 100 * sliderImages.length + '%'
 sliderField.style.display = 'flex'
 sliderField.style.transition = '0.5s all'
 sliderWrapper.style.overflow = 'hidden'
 
-sliderImages.forEach(slide => {
+const sliderDotsList = document.createElement('ol'),
+			dots = []
+sliderDotsList.classList.add('carousel-indicators')
+slider.insertAdjacentElement('beforeend', sliderDotsList)
+
+
+sliderImages.forEach((slide,i) => {
 	slide.style.width = width
+	createSliderDot(i)
+})
+
+dots.forEach(dot=>{
+	dot.addEventListener('click', e=>{
+		slideIndex = +e.target.getAttribute('data-slide-to')
+		offset = +width.slice(0, width.length - 2) * slideIndex
+		sliderField.style.transform = `translateX(-${offset}px)`
+	})
 })
 
 slider.addEventListener('click', e => {
 	//Next
 	if (e.currentTarget && e.target === slideControlNext) {
-		if (offset === +width.slice(0, width.length - 2) * (sliderImages.length - 1)) {
-			offset = 0
-		} else {
-			offset += +width.slice(0, width.length - 2)
-		}
-		if (slideIndex === sliderImages.length - 1) {
-			slideIndex = 0
-		} else {
-			slideIndex++
-		}
+		//Move image
+		offset === +width.slice(0, width.length - 2) * (sliderImages.length - 1)
+		? offset = 0
+		: offset += +width.slice(0, width.length - 2)
+
+		//Change slide index
+		slideIndex === sliderImages.length - 1? slideIndex = 0 :	slideIndex++
 	}
 	//Prev
 	if (e.currentTarget && e.target === slideControlPrev) {
-		if (offset === 0) {
-			offset = +width.slice(0, width.length - 2) * (sliderImages.length - 1)
-		} else {
-			offset -= +width.slice(0, width.length - 2)
-		}
+		//Move image
+		offset === 0
+		?	offset = +width.slice(0, width.length - 2) * (sliderImages.length - 1)
+		: offset -= +width.slice(0, width.length - 2)
 
-		if (!slideIndex) {
-			slideIndex = sliderImages.length - 1
-		} else {
-			slideIndex--
-		}
+		//Change slide index
+		!slideIndex ?	slideIndex = sliderImages.length - 1 : slideIndex--
 	}
+	dots.forEach(dot=>dot.style.opacity = '0.5')
+	dots[slideIndex].style.opacity = '1'
 	sliderField.style.transform = `translateX(-${offset}px)`
 	showCurrentSlide()
 })
@@ -58,9 +70,19 @@ function showCurrentSlide() {
 	} else {
 		totalSlides.textContent = sliderImages.length
 	}
-	currentSlideImg.textContent = getZero(slideIndex + 1)
-
+	currentSlideImg.textContent = getZero((slideIndex+1))
 	function getZero(num) {
 		return num < 10 ? `0${num}` : num
 	}
+}
+
+function createSliderDot(dotIndex) {
+	const dot = document.createElement('li')
+	dot.setAttribute('data-slide-to', dotIndex)
+	dot.classList.add('dot')
+	sliderDotsList.insertAdjacentElement('beforeend', dot)
+	if (dotIndex == 0) {
+		dot.style.opacity = '1'
+	}
+	dots.push(dot)
 }
